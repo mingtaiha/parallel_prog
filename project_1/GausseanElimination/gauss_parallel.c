db72cb6295f64208da56bdbfdcb7aa7da08685ba
Snowrries <allentung5@gmail.com>
2016-10-26 06:40:44 -0400

    Parallel now working. Over and out.

--- project_1/GausseanElimination/gauss_parallel.c
+++ project_1/GausseanElimination/gauss_parallel.c
@@ -5,7 +5,7 @@
 #include <math.h>
 
 typedef struct matricies {
-	double* Asub; // Sub matrix of A
+	double** Asub; // Sub matrix of A
 	int size_Asub; // The number of rows passed to the worker thread
 	double* q; // Input Matrix q
 	double* y;  //Input Matrix y
@@ -19,7 +19,7 @@ typedef struct matricies {
 //Takes 3 matricies.
 void* worker_thread(void* args) {
 	mx* data = (mx*)args;
-	double* A = data->Asub;
+	double** A = data->Asub;
 	double* q = data->q;
 	double* y = data->y;
 	int n = data->n;
@@ -31,10 +31,9 @@ void* worker_thread(void* args) {
 	int j;
 	for (i = 0; i < size; i++) {
 		for (j = k; j < n; j++) {
-			//printf("Sub operation: %f - %f*%f\n",A[i*n +j], certainRow[j], A[k+i*n] );
-                        A[i*n + j] = A[i*n + j] - certainRow[j] * A[k+i*n];
+                        A[i][j] = A[i][j] - certainRow[j] * A[i][k];
 		}
-		q[krt+i] = q[krt+i] - y[krt+i] * A[i*n];// Commit adjusted q value
+		q[krt+i] = q[krt+i] - y[krt] * A[i][k];// Commit adjusted q value
 	}
 	return NULL;
 }
@@ -59,32 +58,35 @@ int main(int argc, char* argv[]) {
 	pthread_attr_t attr;
 	
 	pthread_attr_init(&attr);
-	double* A = (double*)malloc(n*n);
-        double* q = (double*)malloc(n);
-	for(i = 0; i < n; i++){
-		//printf("[ ");
-		for(k = 0; k < n; k++){
-			A[i*n+k] = (ceil(rand()%200) + 1);
-		//	printf("%f " ,A[i*n+k]);
+	double** A = (double**)malloc(n*sizeof(double*));
+	if(A == NULL){printf("malloc failed");return -1;}
+        double* q = (double*)malloc(n*sizeof(double));
+	if(q == NULL){printf("malloc failed");return -1;}
+	for(k = 0; k < n; k++){
+		if((A[k] = (double*) malloc(n*sizeof(double)))== NULL){
+			printf("malloc failed"); return -1;}
+		for(i = 0; i < n; i++){
+			A[k][i] = (ceil(rand()%200) + 1);
 		}
-		//printf(" ]\n");
 		q[i] = (ceil(rand()%200) + 1);
 	}
 	
-	y = (double*)malloc(n); // Initial values of y are irrelevant, y is overwritten and never read until written to. 
-	
+	y = (double*)malloc(n*sizeof(double)); // Initial values of y are irrelevant, y is overwritten and never read until written to. 
+	if(y == NULL){printf("malloc failed"); return -1;}
+
 	clock_t start = clock(), diff;
 	mx* tArgs = (mx*)malloc(4 * sizeof(mx));
 	pthread_t *thread_handles = (pthread_t*) calloc(8,sizeof(pthread_t));
 	pthread_t *backup_handles = (pthread_t*) calloc(8,sizeof(pthread_t));
 		//	Parallel implementation: 
 	for (k = 0; k < n; k ++ ) { // Incrementing Rows
-		for (i = k+n*k; i < n*(k + 1); i++) { // Incrementing Columns
-			A[i] = A[i] / A[k+n*k];//Divide row by first number
+		for (i = k+1; i < n; i++) { // Incrementing Columns
+			A[k][i] = A[k][i] / A[k][k];//Divide row by first number
 		}
-		
+		y[k] = q[k] / A[k][k];// Store adjusted q value in y
+		A[k][k] = 1;
+
 		nk1 = n-k-1;
-		y[k] = q[k] / A[k + n*k];// Store adjusted q value in y
 		//	Hand each thread n/4 consecutive rows; one thread per CPU, and process the elimination.
 		rows = (nk1) / 4;
 		leftover = n-k;
@@ -98,43 +100,44 @@ int main(int argc, char* argv[]) {
 		else{
 			rows = nk1/4; // Guaranteed to be non zero
 			actualThreads = 4;
-			leftover = nk1 - rows*(actualThreads-2);
+			leftover = nk1 - rows*(actualThreads-1);
 		}
-
-		for (threads = 0; threads < actualThreads; threads++) {
-			if (threads == 3) 
-				rows = leftover; 
-			tArgs[threads].Asub = &A[n*(k+rt+1)];
-		//	The pointer to the first array entry of the worker thread's submatrix
-			tArgs[threads].size_Asub = rows;
-		//	Number of rows of said submatrix
-			tArgs[threads].q = q;//Input matrix q (Passed as pointer to first entry of array)
-			tArgs[threads].y = y;//Input matrix y (Passed as pointer to first entry of array)
-			tArgs[threads].k = k;//Current row entry
-			tArgs[threads].krt = k + rt;//Was once k+row*thread
-			tArgs[threads].n = n;//Size of Original matrix
-			tArgs[threads].certainRow = &A[n*k];// pointer to matrix entry at which the divisor row is
-
-		//	Should be threadsafe, as no entry is modified by more than one worker.
-			if(pthread_create(&thread_handles[threads], &attr, &worker_thread,
-				(void *)&tArgs[threads])!= 0){
-				backup_handles[threads] = thread_handles[threads];
-				printf("pthread_create error at: %d\n", threads);
-			}
-			else {
-				//printf("pthread_create success; thread: %d\n", threads);
-			}
-			rt += rows;
-		}
-		//	Wait for all threads to finish before proceeding
-		for (threads = 0; threads<actualThreads; threads++) {
-			if(backup_handles[threads]!=0){
-				//printf("Attempting pthread join of thread: %d\n", threads);
-				if (pthread_join(backup_handles[threads], &res) != 0) {
-					printf("thread %d failed", threads);
+		if(k>n-1){
+			for (threads = 0; threads < actualThreads; threads++) {
+				if (threads == 3) 
+					rows = leftover; 
+				tArgs[threads].Asub = &A[k+rt+1];
+			//	The pointer to the first array entry of the worker thread's submatrix
+				tArgs[threads].size_Asub = rows;
+			//	Number of rows of said submatrix
+				tArgs[threads].q = q;//Input matrix q (Passed as pointer to first entry of array)
+				tArgs[threads].y = y;//Input matrix y (Passed as pointer to first entry of array)
+				tArgs[threads].k = k;//Current row entry
+				tArgs[threads].krt = k + rt;//Was once k+row*thread
+				tArgs[threads].n = n;//Size of Original matrix
+				tArgs[threads].certainRow = A[k];// pointer to matrix entry at which the divisor row is
+	
+			//	Should be threadsafe, as no entry is modified by more than one worker.
+				if(pthread_create(&thread_handles[threads], &attr, &worker_thread,
+					(void *)&tArgs[threads])!= 0){
+					backup_handles[threads] = thread_handles[threads];
+					printf("pthread_create error at: %d\n", threads);
 				}
+				else {
+					//printf("pthread_create success; thread: %d\n", threads);
+				}
+				rt += rows;
 			}
-			backup_handles[threads] = 0;
+			//	Wait for all threads to finish before proceeding
+			for (threads = 0; threads<actualThreads; threads++) {
+				if(backup_handles[threads]!=0){
+					//printf("Attempting pthread join of thread: %d\n", threads);
+					if (pthread_join(backup_handles[threads], &res) != 0) {
+						printf("thread %d failed", threads);
+					}
+				}
+				backup_handles[threads] = 0;
+			}
 		}
 	}
 
