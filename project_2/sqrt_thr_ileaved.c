63d589b8d25d41293731d5f29f18fe9ab1faf423
mingtaiha <ming.tai.ha@gmail.com>
2016-11-20 14:18:56 -0500

    Adding interleaved threading implementation

---
+++ project_2/sqrt_thr_ileaved.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <pthread.h>
+#include "array_gen.h"
+
+#define NUM_THREADS 4
+#define ARR_SIZE 30000000
+#define TOLERANCE 0.0001
+
+
+
+typedef struct thr_arg {
+	int start;
+	pthread_t thr_id;
+} thr_arg;
+
+
+
+float * arr;
+float * sqrt_arr;
+float * appx_arr;
+thr_arg * arg_arr;
+
+
+void define_args() {
+
+	arg_arr = malloc(NUM_THREADS * sizeof(thr_arg));
+	
+	int i;
+	for (i = 0; i < NUM_THREADS; i++) {
+		arg_arr[i].start = i;
+	}
+}
+
+void * newton_thr(void * args) {
+
+	thr_arg * tinfo = (thr_arg *) args;
+	int start = tinfo->start;
+	
+	int i;
+	for (i = start; i < ARR_SIZE; i+=NUM_THREADS) {
+		float x = arr[i];
+		while (fabs(x - sqrt_arr[i]) > TOLERANCE) {
+			x = x - (((x * x) - arr[i]) / (2 * x));
+		}
+		//appx_arr[i] = x;
+	}
+}
+
+int main() {
+
+	char arr_name[] = "arr_30m.dat";
+	char sqrt_arr_name[] = "sqrt_arr_30m.dat";
+	arr = read_array(arr_name);
+	sqrt_arr = read_array(sqrt_arr_name);	
+	appx_arr = (float *) malloc(ARR_SIZE * sizeof(float));
+
+	define_args();
+
+
+	clock_t start, end;
+	double cpu_time_used;
+	int i, s;
+
+	start = clock();
+	for (i = 0; i < NUM_THREADS; i++) {
+		pthread_create(&arg_arr[i].thr_id, NULL, &newton_thr, &arg_arr[i]);
+	}
+	for (i = 0; i < NUM_THREADS; i++) {
+		pthread_join(arg_arr[i].thr_id, NULL);
+	}
+	end = clock();
+	cpu_time_used = (((double) end) - ((double) start)) / CLOCKS_PER_SEC;
+
+	printf("Parallel Runtime: %f\n", cpu_time_used);
+
+
+	
+//	print_array(sqrt_arr);
+//	printf("\n");
+//	print_array(appx_arr);
+
+	free(arr);
+	free(sqrt_arr);
+	free(appx_arr);
+	return 0;
+}
+
