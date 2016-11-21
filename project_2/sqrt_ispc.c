4f2e540b76368a8efd9e02eb6e820c5ff3d5403e
Snowrries <Snowrries@users.noreply.github.com>
2016-11-21 02:07:25 -0500

    Will test shortly.
    Ispc still compiling locally...

--- project_2/sqrt_ispc.c
+++ project_2/sqrt_ispc.c
@@ -11,17 +11,27 @@ float * arr;
 float * sqrt_arr;
 float * appx_arr;
 float tolerance = 0.0001;
+float size = SIZE;
 
 
-int main() {
-
+int main(int argc, char* argv[] ) {
+	if(argc!=2){
+		printf("Please enter only one integer argument, the number of cores.");
+		return -1;
+	}
+	int cores = atoi(argv[1]);
+	//Could error check on cores here.
+	if(cores < 1 || cores > 8){
+		printf("Invalid number of cores.");
+		return -1;
+	}
 	char arr_name[] = "arr_15m.dat";
 	char sqrt_arr_name[] = "sqrt_arr_15m.dat";
 	arr = read_array(arr_name);
 	sqrt_arr = read_array(sqrt_arr_name);	
 	appx_arr = (float *) calloc(SIZE * sizeof(float));
 	clock_t begin = clock();
-	newton_ispc();
+	newton_ispc(size, 4, arr, appx_arr);
 	clock_t end = clock();
 	double time = (double) (end-begin) / CLOCKS_PER_SEC;
 	
