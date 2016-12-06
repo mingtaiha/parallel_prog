e942faf93b410626b2c989c3969f216fa3d5ae3a
chb50 <cedricblake48@gmail.com>
2016-12-06 17:49:53 -0500

    added flag to test if vertex has been visited, cleaned up checkGraph func

--- project_3/part3/graph.c
+++ project_3/part3/graph.c
@@ -11,12 +11,14 @@
 
 #include "linklist.h"
 #include "queue.h"
+#include "graph.h"
 
 
 //garuntees that every vertex is connected to at least 1 other vertex
 //returns the corrected graph
-int** checkGraph(int** g, int size) { //for simplicity, change every value around isolated value to (not -1)
-	int** graph;
+//NOTE: currenty this does not accout for isolated segments, just individual verticies
+vertex** checkGraph(vertex** g, int size) { //for simplicity, change every value around isolated value to (not -1)
+	vertex** graph;
 	graph = g;
 	int i = 0;
 	for(;i < size; ++i) {
@@ -24,69 +26,99 @@ int** checkGraph(int** g, int size) { //for simplicity, change every value aroun
 		for(;j < size; ++j) {
 			if(i == 0) { //top row
 				if (j == 0) { //top left corner
-					if (graph[i+1][j] == -1 && graph[i][j+1] == -1 && graph[i+1][j+1] == -1) {
-						graph[i+1][j] = rand() % 100; //this is 100 (from 0 to 99)
-						graph[i][j+1] = rand() % 100; //this is 100 (from 0 to 99)
-						graph[i+1][j+1] = rand() % 100; //this is 100 (from 0 to 99)
+					if (graph[i+1][j].val == -1 && graph[i][j+1].val == -1 && graph[i+1][j+1].val == -1) {
+						graph[i+1][j].val = rand() % 100; //this is 100 (from 0 to 99)
+						graph[i][j+1].val = rand() % 100; //this is 100 (from 0 to 99)
+						graph[i+1][j+1].val = rand() % 100; //this is 100 (from 0 to 99)
 					}
-				} else if (j == size) { //top right corner
-					if (graph[i+1][j] == -1 && graph[i][j-1] == -1 && graph[i+1][j-1] == -1) {
-						graph[i+1][j] = rand() % 100; 
-						graph[i][j-1] = rand() % 100; 
-						graph[i+1][j-1] = rand() % 100; 
+				} else if (j == size - 1) { //top right corner
+					if (graph[i+1][j].val == -1 && graph[i][j-1].val == -1 && graph[i+1][j-1].val == -1) {
+						graph[i+1][j].val = rand() % 100; 
+						graph[i][j-1].val = rand() % 100; 
+						graph[i+1][j-1].val = rand() % 100; 
 					}
 				} else { //just on top row
-					if (graph[i][j-1] == -1 && graph[i][j+1] == -1 &&
-						graph[i+1][j-1] == -1 && graph[i+1][j] == -1 && graph[i+1][j+1] == -1) {
+					if (graph[i][j-1].val == -1 && graph[i][j+1].val == -1 &&
+						graph[i+1][j-1].val == -1 && graph[i+1][j].val == -1 && graph[i+1][j+1].val == -1) {
 
-						graph[i][j-1] = rand() % 100; 
-						graph[i][j+1] = rand() % 100; 
-						graph[i+1][j-1] = rand() % 100;
-						graph[i+1][j] = rand() % 100; 
-						graph[i+1][j+1] = rand() % 100; 
+						graph[i][j-1].val = rand() % 100; 
+						graph[i][j+1].val = rand() % 100; 
+						graph[i+1][j-1].val = rand() % 100;
+						graph[i+1][j].val = rand() % 100; 
+						graph[i+1][j+1].val = rand() % 100; 
 
 					}
 				}
-			} else if (i == size) {
+			} else if (i == size - 1) {
 				if (j == 0) { //bot left corner
-					if (graph[i-1][j] == -1 && graph[i][j+1] == -1 && graph[i-1][j+1] == -1) {
-						graph[i-1][j] = rand() % 100; //this is 100 (from 0 to 99)
-						graph[i][j+1] = rand() % 100; //this is 100 (from 0 to 99)
-						graph[i-1][j+1] = rand() % 100; //this is 100 (from 0 to 99)
+					if (graph[i-1][j].val == -1 && graph[i][j+1].val == -1 && graph[i-1][j+1].val == -1) {
+						graph[i-1][j].val = rand() % 100; //this is 100 (from 0 to 99)
+						graph[i][j+1].val = rand() % 100; //this is 100 (from 0 to 99)
+						graph[i-1][j+1].val = rand() % 100; //this is 100 (from 0 to 99)
 					}
-				} else if (j == size) { //bot right corner
-					if (graph[i-1][j] == -1 && graph[i][j-1] == -1 && graph[i-1][j-1] == -1) {
-						graph[i-1][j] = rand() % 100; 
-						graph[i][j-1] = rand() % 100; 
-						graph[i-1][j-1] = rand() % 100; 
+				} else if (j == size - 1) { //bot right corner
+					if (graph[i-1][j].val == -1 && graph[i][j-1].val == -1 && graph[i-1][j-1].val == -1) {
+						graph[i-1][j].val = rand() % 100; 
+						graph[i][j-1].val = rand() % 100; 
+						graph[i-1][j-1].val = rand() % 100; 
 					}
 				} else { //just on bot row
-					if (graph[i][j-1] == -1 && graph[i][j+1] == -1 &&
-						graph[i-1][j-1] == -1 && graph[i-1][j] == -1 && graph[i-1][j+1] == -1) {
+					if (graph[i][j-1].val == -1 && graph[i][j+1].val == -1 &&
+						graph[i-1][j-1].val == -1 && graph[i-1][j].val == -1 && graph[i-1][j+1].val == -1) {
 
-						graph[i][j-1] = rand() % 100; 
-						graph[i][j+1] = rand() % 100; 
-						graph[i-1][j-1] = rand() % 100;
-						graph[i-1][j] = rand() % 100; 
-						graph[i-1][j+1] = rand() % 100; 
+						graph[i][j-1].val = rand() % 100; 
+						graph[i][j+1].val = rand() % 100; 
+						graph[i-1][j-1].val = rand() % 100;
+						graph[i-1][j].val = rand() % 100; 
+						graph[i-1][j+1].val = rand() % 100; 
 
 					}
 				}
 			} else {
-				if (graph[i-1][j-1] == -1 && graph[i-1][j] == -1 && graph[i-1][j+1] == -1 &&
-					graph[i][j-1] == -1 && graph[i][j+1] == -1 &&
-					graph[i+1][j-1] == -1 && graph[i+1][j] == -1 && graph[i+1][j+1] == -1) {
+				if (j == 0) { //mid row, left side of graph
+					if(graph[i-1][j].val == -1 && graph[i-1][j+1].val == -1 &&
+						graph[i][j+1].val == -1 &&
+						graph[i+1][j].val == -1 && graph[i+1][j+1].val == -1) {
 
-					graph[i-1][j-1] = rand() % 100;
-					graph[i-1][j] = rand() % 100;
-					graph[i-1][j+1] = rand() % 100;
+						graph[i-1][j].val = rand() % 100;
+						graph[i-1][j+1].val = rand() % 100;
 
-					graph[i][j-1] = rand() % 100;
-					graph[i][j+1] = rand() % 100;
+						graph[i][j+1].val = rand() % 100;
 
-					graph[i+1][j-1] = rand() % 100;
-					graph[i+1][j] = rand() % 100;
-					graph[i+1][j+1] = rand() % 100;
+						graph[i+1][j].val = rand() % 100;
+						graph[i+1][j+1].val = rand() % 100;
+					}
+
+				} else if (j == size - 1) { //mid row, right side of graph
+					if(graph[i-1][j-1].val == -1 && graph[i-1][j].val == -1 &&
+						graph[i][j-1].val == -1 &&
+						graph[i+1][j-1].val == -1 && graph[i+1][j].val == -1) {
+
+						graph[i-1][j-1].val = rand() % 100;
+						graph[i-1][j].val = rand() % 100;
+
+						graph[i][j-1].val = rand() % 100;
+
+						graph[i+1][j-1].val = rand() % 100;
+						graph[i+1][j].val = rand() % 100;
+					}
+
+				} else { //landlocked element
+					if (graph[i-1][j-1].val == -1 && graph[i-1][j].val == -1 && graph[i-1][j+1].val == -1 &&
+						graph[i][j-1].val == -1 && graph[i][j+1].val == -1 &&
+						graph[i+1][j-1].val == -1 && graph[i+1][j].val == -1 && graph[i+1][j+1].val == -1) {
+
+						graph[i-1][j-1].val = rand() % 100;
+						graph[i-1][j].val = rand() % 100;
+						graph[i-1][j+1].val = rand() % 100;
+
+						graph[i][j-1].val = rand() % 100;
+						graph[i][j+1].val = rand() % 100;
+
+						graph[i+1][j-1].val = rand() % 100;
+						graph[i+1][j].val = rand() % 100;
+						graph[i+1][j+1].val = rand() % 100;
+					}
 				}
 			}
 		}
@@ -95,12 +127,20 @@ int** checkGraph(int** g, int size) { //for simplicity, change every value aroun
 	return graph;
 }
 
-void printGraph(int** graph, int size) {
+void printGraph(vertex** graph, int size) {
 	int i = 0;
 	for(;i < size; ++i) {
 		int j = 0;
 		for(;j < size; ++j) {
-			printf("%d, ", graph[i][j]);
+			if (graph[i][j].val != -1) {
+				if (graph[i][j].val < 10) {
+					printf("%d  ", graph[i][j].val);
+				} else {
+					printf("%d ", graph[i][j].val);
+				}
+			} else {
+				printf("   ");
+			}
 		}
 		printf("\n");
 	}
@@ -109,24 +149,31 @@ void printGraph(int** graph, int size) {
 //for testing, keep to 10 by 10 grid
 
 int main(int argc, char* const argv[]) {
-	int n = atoi(argv[1]); //dimensions of generated array
-	int* graph[n];
+	int n;
+	if(argv[1]) {
+		n = atoi(argv[1]); //dimensions of generated array
+	} else {
+		printf("no input!\n");
+		return 0;
+	}
+	vertex* graph[n];
 
-	int** graphTwo; //stores result of checkGraph func
+	vertex** graphTwo; //stores result of checkGraph func
 
 	srand(time(NULL));
 
 	int i = 0;
 	for(; i < n; ++i) {
-		int* a = malloc(sizeof(int) * n);
+		vertex* a = malloc(sizeof(vertex) * n);
 		int j = 0;
 		for(; j < n; ++j) {
-			int rando = rand() % 120; //this is 120 values (from 0 to 119),
-			if (rando <= 100) { //gives -1 more chance to appear
-				a[j] = rando; 
+			int rando = rand() % 150; //this is 150 values (from 0 to 149),
+			if (rando < 100) { //gives -1 more chance to appear
+				a[j].val = rando; 
 			} else {
-				a[j] = -1; //too lazy to find how to include -1 in range when generating random number
+				a[j].val = -1; //too lazy to find how to include -1 in range when generating random number
 			}
+			a[j].vflag = 0;
 		}
 
 		graph[i] = a;
---
+++ project_3/part3/graph.h
@@ -0,0 +1,12 @@
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+typedef struct vertex {
+	int val;
+	int vflag;
+} vertex;
+
+//vertex** checkGraph(vertex** graph, int size);
+void printGraph(vertex** graph, int size);
+
+#endif
