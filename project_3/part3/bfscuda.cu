b8d0d8e8e5f0ee1002d112bdfc4b9dbb548d8b22
chb50 <cedricblake48@gmail.com>
2016-12-16 04:09:23 -0500

    running for 10000 elements (100*100)

--- project_3/part3/bfscuda.cu
+++ project_3/part3/bfscuda.cu
@@ -22,11 +22,12 @@
 		} \
     } while (0)
 
-
 #pragma once
 #ifdef __INTELLISENSE__
 void __syncthreads();
 int atomicOr(int*, int);
+int atomicExch(int*, int);
+int atomicAdd(int*, int);
 #endif
 
 typedef struct vertex {
@@ -40,57 +41,6 @@ typedef struct Node {
 	int* coord; //used to find value on graph array
 	struct Node* next;
 } Node;
-
-//pop from the queue
-__device__ Node queuePop(Node** headPointer) {
-	Node* holder = *headPointer; //holds value to be referenced later
-
-	if (*headPointer == NULL) { //if the link list is empty
-		printf("Nothing to pop!\n");
-		Node returnVal;
-		returnVal.val = -1;
-		returnVal.coord = NULL;
-		return returnVal;
-	}
-
-	if ((*headPointer)->next == NULL) { //if there was only 1 element in the link list
-		Node returnVal;
-		returnVal.val = (*headPointer)->val;
-		returnVal.coord = (*headPointer)->coord;
-		*headPointer = NULL;
-		free(holder);
-		return returnVal;
-	}
-
-	Node* iter = (*headPointer)->next;
-	while (1) {
-		if (iter->next == NULL) {
-			(*headPointer)->next = NULL;
-			Node returnVal;
-			returnVal.val = iter->val;
-			returnVal.coord = iter->coord;
-			free(iter);
-			*headPointer = holder;
-			return returnVal;
-		}
-		*headPointer = iter; //holder always stays 1 step behind iter
-		iter = iter->next;
-	}
-}
-
-__device__ void queuePush(Node** headPointer, int pushVal, int* pushCoord) { //set pushCoord to NULL if you dont care about coord value
-	Node* pushNode = (Node*)malloc(sizeof(Node));
-	pushNode->val = pushVal;
-	pushNode->coord = pushCoord;
-	if (*headPointer == NULL) { //if there is nothing in the queue initially
-		pushNode->next = NULL;
-	}
-	else {
-		pushNode->next = *headPointer;
-	}
-	*headPointer = pushNode;
-	return;
-}
 
 
 //graph.c functions
@@ -277,44 +227,8 @@ void genGraph(vertex*** graph, int size) {
 	*graph = checkGraph(*graph, size);
 }
 
-
-__device__ //altered to account for threads
-Node bfsStart(vertex** graph, int size) {
-	Node start;
-	int i = 0;
-	for(;i < size; ++i) {
-		for (int j = 0; j < size; ++j) {
-			__syncthreads();
-			if ((*graph)[i*size + j].val != -1 && (*graph)[i*size+j].vflag == 0) { //thread has already occupied this start value
-				atomicOr(&((*graph)[i*size + j].vflag), 1); //atomic Or sets flag, removes race condition
-				int* holder = (int*)malloc(sizeof(*holder) * 2);
-				*holder = i;
-				*(holder + 1) = j;
-				start.val = (*graph)[i*size + j].val;
-				start.coord = holder;
-				start.next = NULL;
-				goto funcEnd; //TODO: changed this, now getting seg faults
-			}
-		}
-	}
-	funcEnd:
-	return start;
-}
-
-__device__
-void pushNode(vertex** graph, Node** qhead, int row, int col, int size, int** storageArray) {
-	int val = (*graph)[row*size + col].val;
-	**storageArray = (*graph)[row*size + col].val; //place val into storage array atomically
-	*storageArray = *storageArray + 1;
-	int* holder = (int*)malloc(sizeof(*holder) * 2);
-	*holder = row;
-	*(holder+1) = col;
-	queuePush(qhead, val, holder);
-}
-
 //to be ran on each individual thread
-__device__
-void bfsCuda(vertex** graph, int size, Node currNode, Node** qhead, int** storageArray) {
+__device__ void bfsCuda(vertex* graph, int size, Node currNode) {
 
 	//queue used to store the value that you will move to next
 	//the value that we move to is the "coord" member variable in Node struct
@@ -323,154 +237,113 @@ void bfsCuda(vertex** graph, int size, Node currNode, Node** qhead, int** storag
 	//NOTE: coord[0] is the row, coord[1] is the col
 
 	Node next = currNode; //this will change in while loop
+	int r = *(next.coord);
+	int c = *(next.coord + 1);
 
-	while (1) {
-		int deadEndFlag = 1; //flag for testing if all verticies around a vetex have been visited
-
-		int r = *(next.coord);
-		int c = *(next.coord+1);
+	if (graph[r*size + c].val != -1) {
 
 		//dealing with 1d array here, so have to address based on size of row
-		if(r - 1 >= 0) { //if not first row
-			if((*graph)[(r-1)*size + c].val != -1 && (*graph)[(r-1)*size + c].vflag == 0) {
-				(*graph)[(r - 1)*size + c].vflag = 1;
-				pushNode(graph, qhead, r-1, c, size, storageArray);
-				deadEndFlag = 0;
+		if (r - 1 >= 0) { //if not first row
+			if (graph[(r - 1)*size + c].val != -1 && graph[(r - 1)*size + c].vflag == 0) {
+				graph[(r - 1)*size + c].vflag = 1;
 			}
-			if(c - 1 >= 0) { //if not first row, not first col
-				if((*graph)[(r - 1)*size + (c-1)].val != -1 && (*graph)[(r - 1)*size + (c - 1)].vflag == 0) {
-					(*graph)[(r - 1)*size + (c - 1)].vflag = 1;
-					pushNode(graph, qhead, r-1, c-1, size, storageArray);
-					deadEndFlag = 0;
+			if (c - 1 >= 0) { //if not first row, not first col
+				if (graph[(r - 1)*size + (c - 1)].val != -1 && graph[(r - 1)*size + (c - 1)].vflag == 0) {
+					graph[(r - 1)*size + (c - 1)].vflag = 1;
 				}
-				if((*graph)[(r)*size + (c - 1)].val != -1 && (*graph)[(r)*size + (c - 1)].vflag == 0) {
-					(*graph)[(r)*size + (c - 1)].vflag = 1;
-					pushNode(graph, qhead, r, c-1, size, storageArray);
-					deadEndFlag = 0;
+				if (graph[(r)*size + (c - 1)].val != -1 && graph[(r)*size + (c - 1)].vflag == 0) {
+					graph[(r)*size + (c - 1)].vflag = 1;
 				}
-			} 
+			}
 			if (c + 1 < size) { //not first row, not last col
-				if((*graph)[(r-1)*size + (c + 1)].val != -1 && (*graph)[(r - 1)*size + (c + 1)].vflag == 0) {
-					(*graph)[(r - 1)*size + (c + 1)].vflag = 1;
-					pushNode(graph, qhead, r-1, c+1, size, storageArray);
-					deadEndFlag = 0;
+				if (graph[(r - 1)*size + (c + 1)].val != -1 && graph[(r - 1)*size + (c + 1)].vflag == 0) {
+					graph[(r - 1)*size + (c + 1)].vflag = 1;
 				}
-				if((*graph)[(r)*size + (c + 1)].val != -1 && (*graph)[(r)*size + (c + 1)].vflag == 0) {
-					(*graph)[(r)*size + (c + 1)].vflag = 1;
-					pushNode(graph, qhead, r, c+1, size, storageArray);
-					deadEndFlag = 0;
+				if (graph[(r)*size + (c + 1)].val != -1 && graph[(r)*size + (c + 1)].vflag == 0) {
+					graph[(r)*size + (c + 1)].vflag = 1;
 				}
 			}
 		}
-		if(r + 1 < size) { //not last row
-			if((*graph)[(r+1)*size + (c)].val != -1 && (*graph)[(r + 1)*size + (c)].vflag == 0) {
-				(*graph)[(r + 1)*size + (c)].vflag = 1;
-				pushNode(graph, qhead, r+1, c, size, storageArray);
-				deadEndFlag = 0;
+		if (r + 1 < size) { //not last row
+			if (graph[(r + 1)*size + (c)].val != -1 && graph[(r + 1)*size + (c)].vflag == 0) {
+				graph[(r + 1)*size + (c)].vflag = 1;
 			}
-			if(c - 1 >= 0) { //not last row, not first col
-				if((*graph)[(r)*size + (c-1)].val != -1 && (*graph)[(r)*size + (c - 1)].vflag == 0) {
-					(*graph)[(r)*size + (c - 1)].vflag = 1;
-					pushNode(graph, qhead, r, c-1, size, storageArray);
-					deadEndFlag = 0;
+			if (c - 1 >= 0) { //not last row, not first col
+				if (graph[(r)*size + (c - 1)].val != -1 && graph[(r)*size + (c - 1)].vflag == 0) {
+					graph[(r)*size + (c - 1)].vflag = 1;
 				}
-				if((*graph)[(r+1)*size + (c - 1)].val != -1 && (*graph)[(r + 1)*size + (c - 1)].vflag == 0) {
-					(*graph)[(r + 1)*size + (c - 1)].vflag = 1;
-					pushNode(graph, qhead, r+1, c-1, size, storageArray);
-					deadEndFlag = 0;
+				if (graph[(r + 1)*size + (c - 1)].val != -1 && graph[(r + 1)*size + (c - 1)].vflag == 0) {
+					graph[(r + 1)*size + (c - 1)].vflag = 1;
 				}
 			}
-			if(c + 1 < size) { //not last row, not last col
-				if((*graph)[(r)*size + (c+1)].val != -1 && (*graph)[(r)*size + (c + 1)].vflag == 0) {
-					(*graph)[(r)*size + (c + 1)].vflag = 1;
-					pushNode(graph, qhead, r, c+1, size, storageArray);
-					deadEndFlag = 0;
+			if (c + 1 < size) { //not last row, not last col
+				if (graph[(r)*size + (c + 1)].val != -1 && graph[(r)*size + (c + 1)].vflag == 0) {
+					graph[(r)*size + (c + 1)].vflag = 1;
 				}
-				if ((*graph)[(r+1)*size + (c + 1)].val != -1 && (*graph)[(r + 1)*size + (c + 1)].vflag == 0) {
-					(*graph)[(r + 1)*size + (c + 1)].vflag = 1;
-					pushNode(graph, qhead, r+1, c+1, size, storageArray);
-					deadEndFlag = 0;
+				if (graph[(r + 1)*size + (c + 1)].val != -1 && graph[(r + 1)*size + (c + 1)].vflag == 0) {
+					graph[(r + 1)*size + (c + 1)].vflag = 1;
 				}
-			}
-		}
-
-		//once each thread has marked surrounding nodes, then they select which node to go to next
-		__syncthreads();
-
-
-		//ACTUALLY, dont think this is a problen, if a thread flags a node, then the
-		//thread that comes after it will see that it has already been flagged.
-		//becaue the flag only needs to be set once, synchronization isnt really a problem as
-		//far as the graph is concerned. just need to synch when storing found value into storage array
-		//TODO: will need to have another check because a thread that has finished its check
-		//does not see that its adjacent nodes have been
-
-		//base case
-		//base case check
-		if(!deadEndFlag) { //need to do recursion
-			next = queuePop(qhead);
-		} else {
-			if (*qhead != NULL) {
-				next = queuePop(qhead);
-			} else {
-				break;
 			}
 		}
 	}
 }
 
-__global__	void bfsCudaKernel(vertex* graph1d, int size, int* storageArray) {
-	//int tIndex = threadIdx.x + blockIdx.x*blockDim.x;
+__global__	void bfsCudaKernel(vertex* graph1d, int size) {
+	int tIndex = threadIdx.x + blockIdx.x*blockDim.x;
+	int numThreads = blockDim.x + gridDim.x;
 
 	//each thread should have there own start place and queue
 	//create queue
-	Node* qheadbuff = nullptr;
-	//initialize queue for each thread
-	Node start = bfsStart(&graph1d, size);
-
-	__syncthreads();
 
 	//graph should be divided based on block indexing
-	bfsCuda(&graph1d, size, start, &qheadbuff, &storageArray);
+	//bfsCuda(graph1d, size, startHolder[tIndex], qheadbuff);
 
+	Node start;
+	int* holder = (int*)malloc(sizeof(*holder) * 2);
+	*holder = graph1d[tIndex].coordX;
+	*(holder + 1) = graph1d[tIndex].coordY;
+	start.val = graph1d[tIndex].val;
+	start.coord = holder;
+	start.next = nullptr;
 
+	for (int i = tIndex; i < size*size; ++i) {
+		bfsCuda(graph1d, size, start);
+	}
+
+	free(holder);
+	__syncthreads();
 
 }
 
 
 //fuction to change 2d array into 1d array
-vertex* flatten(vertex** array2d, int size2d) {
-	vertex* array1d = (vertex*)malloc(sizeof(vertex)*size2d*size2d);
+void flatten(vertex** array2d, vertex** array1d, int size2d) {
 
 	for (int i = 0; i < size2d; ++i) {
 		for (int j = 0; j < size2d; ++j) {
-			array1d[j + i*size2d] = array2d[i][j];
+			(*array1d)[j + i*size2d] = array2d[i][j];
 			//store the coordinates of the flattened array
-			array1d[j + i*size2d].coordX = i;
-			array1d[j + i*size2d].coordY = j;
+			(*array1d)[j + i*size2d].coordX = i;
+			(*array1d)[j + i*size2d].coordY = j;
 		}
 	}
-
-	return array1d;
 }
 
-
-vertex** expand(vertex* array1d, int size2d) {
-	vertex** array2d = (vertex**)malloc(sizeof(vertex*)*size2d*size2d);
+void expand(vertex*** graph, vertex* array1d, int size2d) {
 
 	for (int i = 0; i < size2d; ++i) {
 		for (int j = 0; j < size2d; ++j) {
-			array2d[i][j] = array1d[j + i*size2d];
+			(*graph)[i][j].vflag = array1d[j + i*size2d].vflag;
 		}
 	}
-
-	return array2d;
 }
 
 int main(int argc, char* argv[]) {
-	int graphSize = 10;
-	int numBlocks = 1; //try to code for only threads for now
-	int tPerBlock = 1;
+	int graphSize = 100;
+	int numBlocks = 100; //try to code for only threads for now
+	int tPerBlock = 100;
+
+	srand(time(NULL));
 
 	if(graphSize < 1 || numBlocks < 1 || tPerBlock < 1) {
 		printf("invalid input!\n");
@@ -478,58 +351,48 @@ int main(int argc, char* argv[]) {
 	}
 
 	vertex** g;
-	int valSize = sizeof(int) * (graphSize*graphSize);
-	int* valStorage = (int*)malloc(valSize);
  	genGraph(&g, graphSize);
-
-	vertex* g1d = flatten(g, graphSize);
-
-	vertex* d_graph;
-	int* d_valStorage; //store the result here (every thread does this)
 	
+	vertex* g1d = (vertex*)malloc(sizeof(*g1d)*graphSize*graphSize);
+	flatten(g, &g1d, graphSize);
 
+	
+	vertex* d_graph;
+	
+	cudaSetDevice(0);
+	
 	cudaMalloc((void**)&d_graph, sizeof(vertex)*graphSize*graphSize);
-	cudaMalloc((void**)&d_valStorage, valSize);
+
 	
 	cudaMemcpy(d_graph, g1d, sizeof(vertex)*graphSize*graphSize, cudaMemcpyHostToDevice); //move graph to device
+	
+	clock_t begin = clock();
+	bfsCudaKernel<<<numBlocks, tPerBlock>>>(d_graph, graphSize);
+	clock_t end = clock();
 
-	bfsCudaKernel<<<1, tPerBlock>>>(d_graph, graphSize, d_valStorage);
-	cudaCheckErrors("failed");
+	float timeTaken = (float)((end - begin) / (float)CLOCKS_PER_SEC);
 
 	cudaMemcpy(g1d, d_graph, sizeof(vertex)*graphSize*graphSize, cudaMemcpyDeviceToHost);
-	cudaMemcpy(valStorage, d_valStorage, valSize, cudaMemcpyDeviceToHost);
+	cudaCheckErrors("failed");
 
+	expand(&g, g1d, graphSize);
+	
+	/*
 	for (int k = 0; k < graphSize*graphSize; k++) {
 		//printf("%d\n", g1d[k].val);
-		printf("%d\n", g1d[k].vflag);
+		printf("%d, ", g1d[k].val);
 		//printf("%d\n", valStorage[k]);
 	}
+	*/
 
-	g = expand(g1d, graphSize);
-	printGraph(g, graphSize);
-
-	int i = 0;
-	for (; i < graphSize*graphSize; ++i) {
-		if (valStorage[i] == -1) {
-			break;
-		}
-		if (i % 10 != 9) {
-			printf("%d, ", valStorage[i]);
-		}
-		else {
-			printf("%d\n", valStorage[i]);
-		}
-	}
 	printf("\n");
+	printf("time taken: %f\n", timeTaken);
 
 	cudaFree(&d_graph);
-	cudaFree(&d_valStorage);
 
-
+	//printGraph(g, graphSize);
 	free(g);
 	free(g1d);
-	free(valStorage);
-
 	return 0;
 
 }
